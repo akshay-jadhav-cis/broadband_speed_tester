99d86f55810fd35a843c334e0bae47e7ec3a6b18
akshay-jadhav-cis <aj567577@gmail.com>
2026-07-03 18:25:53 +0530

    Use Render backend

--- frontend/src/api/network.js
+++ frontend/src/api/network.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import API_URL from "../config/api";
 
-const BASE_URL = "http://localhost:8000";
+
 
 export async function getNetworkInfo() {
 
     const { data } = await axios.get(
-        `${BASE_URL}/network`
+        `${API_URL}/network`
     );
 
     return data;
